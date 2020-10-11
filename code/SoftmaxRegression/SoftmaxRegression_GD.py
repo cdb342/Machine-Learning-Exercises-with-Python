2cbc9fe43b853592e785e2295c0fcdad809eac4d
db.Chen <61510717+cdb342@users.noreply.github.com>
2020-10-11 14:51:00 +0800

    Update SoftmaxRegression_GD.py

--- code/SoftmaxRegression/SoftmaxRegression_GD.py
+++ code/SoftmaxRegression/SoftmaxRegression_GD.py
@@ -6,10 +6,10 @@ from matplotlib import animation
 train_X:训练集特征集合   train_y:训练集标签集合
 test_X:测试集特征集合    test_y：测试集标签集合
 """
-train_X=np.loadtxt("./Iris/train/x.txt")
-train_y=np.loadtxt("./Iris/train/y.txt")
-test_X=np.loadtxt("./Iris/test/x.txt")
-test_y=np.loadtxt("./Iris/test/y.txt")
+train_X=np.loadtxt("./dataset/Iris/train/x.txt")
+train_y=np.loadtxt("./dataset/Iris/train/y.txt")
+test_X=np.loadtxt("./dataset/Iris/test/x.txt")
+test_y=np.loadtxt("./dataset/Iris/test/y.txt")
 print(train_X,train_y,test_X,test_y)
 """
 创建softmax函数
